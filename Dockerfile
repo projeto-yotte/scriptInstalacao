7fff94bd800954a6993ebeb14b4beb85c4488278
Daniel194 <daniel.rsantos@sptech.school>
2023-11-28 19:35:31 -0300

    mudanca 5

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ ENV MYSQL_DATABASE=yotte
 ENV MYSQL_ROOT_PASSWORD=yotte2023
 
 #Copia o arquivo de configuração do banco para pasta do docker para ser executado
-COPY  ./yotte-bd.sql /docker-entrypoint-initdb.d/
+COPY  ./yotte-bd.sql/ /docker-entrypoint-initdb.d/
 
 #Expõe a porta 3306 do MySQL
 EXPOSE 3306
--- docker-config.sh
+++ docker-config.sh
@@ -1,90 +1,48 @@
 #!/bin/bash
 
-# Configurações do Docker
-    
-echo -e "\033[${RED}m[yotte.u]:${NC} Bem vindo!! Iremos iniciar a instalação"
+# Configurações de cores
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
-sleep 3
-
-echo -e "\033[${RED}m[yotte.u]:${NC} Atualizando e baixando pacotes do programa!" 
-echo -e "\033[${RED}m[yotte.u]:${NC} Aguarde um momento"
+# Início do script de instalação e configuração do Docker
+echo -e "\033[${RED}m[yotte.u]:${NC} Bem-vindo!! Iniciando a instalação e configuração do Docker."
 
 # Atualiza a lista de pacotes
 sudo apt update -y && sudo apt upgrade -y
-
 echo -e "\033[${RED}m[yotte.u]:${NC} Pacotes atualizados!"
 
-sleep 2
-
-echo -e "\033[${RED}m[yotte.u]:${NC}Iniciando instalação e configuração do Docker..."
 # Instalação do Docker
-sudo apt install  docker.io -y
-
-sleep 2
-
+sudo apt install docker.io -y
 echo -e "\033[${RED}m[yotte.u]:${NC} Docker instalado com sucesso!"
 
 # Adiciona o usuário atual ao grupo docker para executar comandos docker sem sudo
+sudo usermod -aG docker $USER
 
 # Reinicia o serviço do Docker para aplicar as alterações
 sudo systemctl start docker
+echo -e "Docker foi instalado e configurado com sucesso. Por favor, faça logout e login novamente para aplicar as alterações do grupo Docker."
 
-echo "Docker foi instalado e configurado com sucesso. Por favor, faça logout e login novamente para aplicar as alterações do grupo Docker."
+# Construção da imagem Docker usando o Dockerfile separado
+echo -e "\033[${RED}m[yotte.u]:${NC} Construindo a imagem Docker"
+sudo docker build -t yotte -f Dockerfile .
+echo -e "\033[${RED}m[yotte.u]:${NC} Imagem Docker construída com sucesso!"
 
-#!/bin/bash
+# Criação do contêiner
+sudo docker run -d --name yotte-container -p 3306:3306 yotte
+echo -e "\033[${RED}m[yotte.u]:${NC} Contêiner do Banco de Dados criado com sucesso!"
 
-
-# Iniciando o script de instalação e configuração do Docker
-
-# Instalação do Docker
-sudo apt update -y && sudo apt upgrade -y
-
-sleep 5
-
-# Verificar se o Docker foi instalado corretamente
-sudo docker --version
-
-sleep 5
-
-# Inicia o serviço do Docker
-sudo systemctl start docker
-sleep 5
-# Habilitar o serviço do Docker
-sudo systemctl enable docker
-
-sleep 5
-echo -e "\033[${RED}m[yotte.u]:${NC} Instalando Mysql"
-# Fazer pull da imagem do MySQL 5.7
-sudo docker pull mysql:latest
-sleep 2
-echo -e "\033[${RED}m[yotte.u]:${NC} Imagem carregada com sucesso!"
-
-sudo docker-compose up -d
-sleep 15
-# Criando container e configurando o MySQL
-# sudo docker run -d -p 3306:3306 --name yotte-container -e "MYSQL_DATABASE=yotte" -e "MYSQL_ROOT_PASSWORD=yotte2023" mysql:latest
-
-# sudo docker run -d --name yotte-container -p 3306:3306 yotte
-
-
-
-echo -e "\033[${RED}m[yotte.u]:${NC} Container do Banco de Dados criado com sucesso!"
-
-# sudo apt update -y
-# Mensagem de confirmação da instalação e configuração do Docker
+# Atualização dos pacotes
+sudo apt update -y
 echo "Docker foi instalado e configurado com sucesso. O Docker está pronto para uso!"
 
+# Execução de comandos no contêiner MySQL
+sudo docker exec -i yotte-container mysql -u root -pyotte2023 < /home/ubuntu/scriptInstalacao/yotte-bd.sql
+echo -e "\033[${RED}m[yotte.u]:${NC} Docker Noct.u executado com sucesso!"
 
-
-
-sleep 15
-# sudo docker exec -i yotte-container mysql -u root -pyotte2023 < /home/ubuntu/scriptInstalacao/yotte-bd.sql
-
- echo -e "\033[${RED}m[yotte.u]:${NC} Docker Noct.u executado com sucesso!"
-
+# Permissões de execução para o script Java
 sudo chmod +x java-install.sh
 
+# Execução do script Java
 ./java-install.sh
 
-sleep 10
-
+sleep 15
