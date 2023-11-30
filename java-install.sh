55b288f9f4a76f3dcec314ce25d2c10f5dee95cf
vicLira <victor.liracarlos@gmail.com>
2023-11-30 14:39:03 -0300

    instalação java mais amigavel

--- java-install.sh
+++ java-install.sh
@@ -8,29 +8,25 @@ jar_url="https://github.com/projeto-yotte/yotte-monitoramento-grupo/raw/main/tar
 # Nome do arquivo JAR após o download
 jar_nome="yotte-monitoramento-jar-with-dependencies.jar"
 
-
 # Atualização e Upgrade
-
-
 sleep 5
+
 # Versão do Java a ser instalada
 JAVA_VERSION=17
 
 if java -version 2>&1 | grep -q "openjdk version \"$JAVA_VERSION"; then
-        echo "Seu Java já está instalado"
+    echo "Seu Java já está instalado"
+else
+    echo "A versão do Java é inferior"
+    echo "Deseja atualizar? [s/n]"
+    read get
+
+    if [ "$get" == "s" ]; then
+        sudo apt install openjdk-${JAVA_VERSION}-jre-headless -y
     else
-        echo "A versão do Java é inferior"
-        echo "Deseja atualizar? [s/n]"
-        read get
-
-        if [ "$get" == "s" ]; then
-            sudo apt install openjdk-${JAVA_VERSION}-jre-headless -y
-        else
-            echo "Finalizando script..."
-        fi
+        echo "Finalizando script..."
     fi
-
-
+fi
 
 sleep 5
 echo -e "\033[${RED}m[yotte.u]:${NC} Baixando o wget!"
@@ -39,16 +35,15 @@ sudo apt install wget -y
 
 echo -e "\033[${RED}m[yotte.u]:${NC} Instalado com sucesso!!"
 
-  sleep 5
+sleep 5
 
-  sudo wget "$jar_url" -O "$jar_nome"
+sudo wget "$jar_url" -O "$jar_nome"
 
+sudo java -jar "$jar_nome"
 
- sudo java -jar "$jar_nome"
+echo -e "\033[${RED}m[yotte.u]:${NC} Java executado!!"
 
- echo -e "\033[${RED}m[yotte.u]:${NC} Java executado!!"
-
- sleep 2
+sleep 2
 
 
 # Clonar o repositórios do projeto
